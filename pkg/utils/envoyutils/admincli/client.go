58c31a9546fe059759678446cbeb73e4b2e33b97
Jenny Shu <jenny.shu@solo.io>
2025-02-27 11:51:45 -0500

    import
    Signed-off-by: Jenny Shu <jenny.shu@solo.io>

--- pkg/utils/envoyutils/admincli/client.go
+++ pkg/utils/envoyutils/admincli/client.go
@@ -18,6 +18,9 @@ import (
 	"github.com/kgateway-dev/kgateway/v2/pkg/utils/kubeutils/portforward"
 	"github.com/kgateway-dev/kgateway/v2/pkg/utils/protoutils"
 	"github.com/kgateway-dev/kgateway/v2/pkg/utils/requestutils/curl"
+
+	// import for side effects; this is needed so we can unmarshal envoy types
+	_ "github.com/kgateway-dev/kgateway/v2/internal/envoyinit/hack/filter_types"
 )
 
 const (
